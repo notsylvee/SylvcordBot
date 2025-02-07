e26a870539d7190128eab57cfa93e075201307d4
Sylvee <79902941+notsylvee@users.noreply.github.com>
2025-02-07 17:11:19 -0500

    fixed join/leave messages

--- src/events/goodbye.js
+++ src/events/goodbye.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "guildMemberRemove",
     async execute(member) {
 
-        const channel = await member.guild.channels.cache.find(channel => channel.id === '1295777661477589114');
+        const channel = await member.guild.channels.cache.find(channel => channel.id === '1337545749235499018');
         await channel.fetch();
         channel.send(`${member.user} left`)
     },
--- src/events/welcome.js
+++ src/events/welcome.js
@@ -1,10 +1,12 @@
 module.exports = {
     name: "guildMemberAdd",
     async execute(member) {
-        const role = await member.guild.roles.cache.find(role => role.id === '1246702854568153129');
-        await member.roles.add(role);
+        const memberRole = await member.guild.roles.cache.find(role => role.id === '1246702854568153129');
+        const OGRole = await member.guild.roles.cache.find(role => role.id === '1280059486454681623');
+        await member.roles.add(memberRole);
+        await member.roles.add(OGRole);
 
-        const channel = await member.guild.channels.cache.find(channel => channel.id === '1295777661477589114');
+        const channel = await member.guild.channels.cache.find(channel => channel.id === '1337545749235499018');
         await channel.fetch();
         channel.send(`${member.user}, welcome to Sylvcord!`)
     },
