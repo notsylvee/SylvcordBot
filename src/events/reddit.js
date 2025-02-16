907541e3ff89c0bf4b937d82ea469c44dcf1c65d
Sylvee <79902941+notsylvee@users.noreply.github.com>
2025-02-15 23:53:35 -0500

    added reddit.js

--- src/events/reddit.js
+++ src/events/reddit.js
@@ -1,8 +1,8 @@
 module.exports = {
     name: "messageCreate",
     async execute(message) {
-      if (message.channel.id !== `1219415504884731994`) return;
-      if (message.author.id !== `729906911486672908`) return;
+      if (message.channel.id !== `1340546248918437919`) return;
+      message.react(`<:sc_upvote:1240066343491866675>`);
       message.react(`<:sc_downvote:1240066368058167367>`);
     },
   };
